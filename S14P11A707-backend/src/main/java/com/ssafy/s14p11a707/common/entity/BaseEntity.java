d7d31a3eb20a394bca4b98314fa45c75caafea54
gjaku1031 <gjaku1031@gmail.com>
2026-01-22 14:58:25 +0900

    feat: add BaseEntity with

---
+++ S14P11A707-backend/src/main/java/com/ssafy/s14p11a707/common/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.ssafy.s14p11a707.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    protected Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    protected Instant updatedAt;
+}
