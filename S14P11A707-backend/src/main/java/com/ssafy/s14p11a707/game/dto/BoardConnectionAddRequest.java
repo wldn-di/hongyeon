10fca3a43a583985ee79d8cbd164fcc21f6f0978
wldn-di <springbom0214@gmail.com>
2026-01-30 01:04:28 +0900

    refactor: 보드 api frontLocalStorage에 관리, db에 한번에 저장하도록 엔드포인트 수정

--- S14P11A707-backend/src/main/java/com/ssafy/s14p11a707/game/dto/BoardConnectionAddRequest.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.s14p11a707.game.dto;
-
-public record BoardConnectionAddRequest(
-        long fromNodeId,
-        long toNodeId,
-        String type
-) {
-}
-
--- S14P11A707-backend/src/main/java/com/ssafy/s14p11a707/game/dto/BoardItemMoveRequest.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.s14p11a707.game.dto;
-
-public record BoardItemMoveRequest(
-        long nodeId,
-        int x,
-        int y
-) {
-}
-
