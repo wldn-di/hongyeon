e8d5f0d5c62bdff6cae278ba05e92daa7fd920ef
wldn-di <springbom0214@gmail.com>
2026-01-31 21:43:14 +0900

    feat: 게임 재 클리어시 클리어수 반영하지 않도록 수정

--- S14P11A707-backend/src/main/java/com/ssafy/s14p11a707/user/entity/User.java
+++ S14P11A707-backend/src/main/java/com/ssafy/s14p11a707/user/entity/User.java
@@ -97,11 +97,17 @@ public class User extends BaseEntity {
         this.totalAttempts++;
     }
 
-    // 게임 완료 시 호출
-    public void addClearStats(long playTime, int score) {
+    // 게임 완료 시 호출(첫 클리어)
+    public void addFirstClearStats(long playTime, int score) {
         this.totalPlayTime += playTime;
         this.totalScore += score;
         this.totalClears++;
+    }
+
+    // 게임 완료 시 호출(재 클리어)
+    public void addReClearStats(long playTime, int score) {
+        this.totalPlayTime += playTime;
+        this.totalScore += score;
     }
 
     // 게임 실패 시 호출
