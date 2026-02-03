4cd926b612547483ed6e9d12275a4b1d4dfcaff2
ktvc0413 <ktvc0413@gmail.com>
2026-02-03 12:35:08 +0900

    feat: SessionSuspectStateRepository 수정

--- S14P11A707-backend/src/main/java/com/ssafy/s14p11a707/game/repository/SessionSuspectStateRepository.java
+++ S14P11A707-backend/src/main/java/com/ssafy/s14p11a707/game/repository/SessionSuspectStateRepository.java
@@ -3,9 +3,14 @@ package com.ssafy.s14p11a707.game.repository;
 import com.ssafy.s14p11a707.game.entity.SessionSuspectState;
 import com.ssafy.s14p11a707.game.entity.SessionSuspectStateId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SessionSuspectStateRepository extends JpaRepository<SessionSuspectState, SessionSuspectStateId> {
 
-    void deleteBySessionId(Long sessionId);
+    @Modifying(clearAutomatically = true)
+    @Query("DELETE FROM SessionSuspectState sss WHERE sss.session.id = :sessionId")
+    int deleteBySessionId(@Param("sessionId") Long sessionId);
 }
 
